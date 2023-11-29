c57bc31a7cf05d0673e4442793d091c9d4a47f64
ryaasishlah <ryaasishlah123@gmail.com>
2023-11-28 16:38:52 -0800

    delete tes

--- type.go
+++ type.go
@@ -103,15 +103,3 @@ type Contact struct {
 	Phone   string `json:"phone" bson:"phone"`
 	Status  bool   `json:"status" bson:"status"`
 }
-
-type rumahmakan struct {
-	ID      int    `json:"id" bson:"id"`
-	Name    string `json:"title" bson:"title"`
-	Subject string `json:"description" bson:"description"`
-	Alamat  string `json:"alamat" bson:"alamat"`
-	Website string `json:"website" bson:"website"`
-	Message string `json:"image" bson:"image"`
-	Email   string `json:"email" bson:"email"`
-	Phone   string `json:"phone" bson:"phone"`
-	Status  bool   `json:"status" bson:"status"`
-}
