df738f1993c08235ce913f4293697f931102c950
ryaasishlah <ryaasishlah123@gmail.com>
2023-11-30 17:45:30 +0700

    update name , delete create response(move)

--- hash.go
+++ hash.go
@@ -2,21 +2,12 @@ package pasabarbackend
 
 import "golang.org/x/crypto/bcrypt"
 
-func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
+func HashPass(password string) (string, error) {
+	bytess, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	return string(bytess), err
 }
 
-func CheckPasswordHash(password, hash string) bool {
+func CompareHashPass(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
-}
-
-func CreateResponse(status bool, message string, data interface{}) Response {
-	response := Response{
-		Status:  status,
-		Message: message,
-		Data:    data,
-	}
-	return response
 }
